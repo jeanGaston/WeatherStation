027e6bd037a66845c3e5a0cbda21d9be68d85c4a
jeanGaston <goncalvesmonnetpaul@gmail.com>
2024-02-06 11:11:32 +0100

    update
    Change pip install in docker file
    Change py to bluepy

--- DataScraper/main.py
+++ DataScraper/main.py
@@ -1,9 +1,16 @@
-import asyncio
-from bleak import BleakScanner
+from bluepy.btle import Scanner
 
-async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
 
-asyncio.run(main())
+scanner = Scanner()
+print("Begin device scan")
+while True:
+    devices = scanner.scan(timeout=3.0)
+
+    for device in devices:
+        print(
+            f"Device found {device.addr} ({device.addrType}), "
+            f"RSSI={device.rssi} dB"
+        )
+        for adtype, description, value in device.getScanData():
+            print(f"  ({adtype}) {description} = {value}")
+
