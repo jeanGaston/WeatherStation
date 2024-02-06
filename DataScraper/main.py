1f11ee26eb7b4c652323955e09e1e7fa1057bd0e
jeanGaston <goncalvesmonnetpaul@gmail.com>
2024-02-06 09:44:38 +0100

    Change to bleak instead of bluepy

--- DataScraper/main.py
+++ DataScraper/main.py
@@ -1,16 +1,9 @@
-from bluepy.btle import Scanner
+import asyncio
+from bleak import BleakScanner
 
+async def main():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
 
-scanner = Scanner()
-print("Begin device scan")
-while True:
-    devices = scanner.scan(timeout=3.0)
-
-    for device in devices:
-        print(
-            f"Device found {device.addr} ({device.addrType}), "
-            f"RSSI={device.rssi} dB"
-        )
-        for adtype, description, value in device.getScanData():
-            print(f"  ({adtype}) {description} = {value}")
-
+asyncio.run(main())
